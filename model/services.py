645063569b21f004181a1f937c2cb53b5315dc77
Joy  Liao <y02121148s@gmail.com>
2022-10-25 13:30:31 +0800

    add logger & adjust some code

--- model/services.py
+++ model/services.py
@@ -1,20 +1,19 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
-from apscheduler.triggers.interval import IntervalTrigger
-from apscheduler.triggers.cron import CronTrigger
-from apscheduler.triggers.date import DateTrigger
 from apscheduler.job import Job
+from apscheduler import triggers
 
 import time
 from datetime import datetime
 from pydantic import BaseModel
 from typing import List, Optional, Callable, Union
+from custom_logger import logger
 
 class JobModel(BaseModel):
     id: str 
     name: str
-    func: Callable
+    _func: Callable
     args: Optional[Union[tuple, list]]
     kwargs: Optional[dict]
     coalesce: Optional[bool] = True
@@ -22,16 +21,14 @@ class JobModel(BaseModel):
     next_run_time: Union[datetime, str]
 
     def check_trigger(trigger):
-
-        if isinstance(trigger, IntervalTrigger):
+        if isinstance(trigger, triggers.interval.IntervalTrigger):
             return'interval'
-        elif isinstance(trigger, CronTrigger):
+        elif isinstance(trigger, triggers.cron.CronTrigger):
             return 'cron'
-        elif isinstance(trigger, DateTrigger):
+        elif isinstance(trigger, triggers.date.DateTrigger):
             return 'date'
 
     def create(schedule_job: Job) -> dict:
-        
         trigger = JobModel.check_trigger(schedule_job.trigger)
         job = JobModel(
             id = schedule_job.id,
@@ -43,15 +40,15 @@ class JobModel(BaseModel):
             coalesce = schedule_job.coalesce,
             next_run_time = schedule_job.next_run_time.strftime("%Y-%m-%d %H:%M:%S")
         )
-        
+
         return job.dict()
         
 class JobsScheduler():
-
     db: str = 'sqlite:///event_scheduler.db'
     thread_num: int = 10
     process_num: int = 10
     max_instances: int = 20
+    timezone: str = "Asia/Taipei"
 
     def __init__(self):    
         self.project_name: str ='apscheduler_jobs'  # project name, one table for one project
@@ -76,18 +73,18 @@ class JobsScheduler():
                 }, 
                 executors=self.executors, 
                 job_defaults=job_defaults, 
-                timezone="Asia/Taipei"
+                timezone=JobsScheduler.timezone
             )
 
         self.start()
 
     def check_job(func):
+        logger.debug(f'Run Function: {func.__name__}')
         def wrapper(self, *args):
             job_id = args[0]
             if self.scheduler.get_job(job_id):
                 return func(self, *args)
-            
-            print(f'[Warning]: {func.__name__}: Job {job_id} Not Exist')
+            logger.warn(f'Job {job_id} Not Exist')
             return None
         return wrapper
 
@@ -99,8 +96,6 @@ class JobsScheduler():
         args: Union[tuple, list] = None,
         kwargs: dict = None
         ):
-
-        print('+ Add Date Job')
         self.scheduler.add_job(
             job,
             id=id,
@@ -127,11 +122,11 @@ class JobsScheduler():
         ):
 
         if exec_immediately:
-            print('+ Run Job Immdiately')
+            logger.info('Run Job Immdiately')
             now = datetime.now()
             self.add_date_job(job, id=id, start_date=now)
 
-        print('+ Add Interval Job')
+        logger.debug('Add Interval Job')
         job = self.scheduler.add_job(
             job, 
             'interval', 
@@ -163,46 +158,36 @@ class JobsScheduler():
 
     @check_job
     def remove_job(self, id):
-        print(f'- Remove Job [{id}]')
         self.scheduler.remove_job(id)
 
     @check_job
     def pause_job(self, id):
-        print(f'- Pause Job [{id}]')
         self.scheduler.pause_job(job_id=id)
     
     @check_job
     def resume_job(self, id):
-        print(f'- Resume Job [{id}]')
         self.scheduler.resume_job(job_id=id)
-
-    @check_job
-    def update_job(self):
-        print(f'- Resume Job [{id}]')
-        pass
     
     def restart(self):
         if not self.scheduler.running:
-            print('*** Scheduler is not running, Restart it now ***')
+            logger.info('Scheduler is not running, Restart it now.')
             self.scheduler.start(paused=True)
-
             for job in self.list_jobs():
                 self.resume_job(job.id)
 
     def start(self):
         if not self.scheduler.running:
-            print('*** Scheduler is not running, Start it now ***')
+            logger.info('Scheduler is not running, Start it now.')
             self.scheduler.start()
     
     def shutdown(self):
         if self.scheduler.running:
-            print('*** Scheduler prepare to shutdown ***')
-            # pause all jobs
-            print('Pause all jobs before shutdown')
+            logger.warning('Scheduler prepare to shutdown.')
+            logger.warning('Pause all jobs before shutdown')
             for job in self.list_jobs():
                 self.pause_job(job.id)
 
-            print('*** Scheduler Shutdown Now ***')
+            logger.warning('Scheduler has been shutdown')
             self.scheduler.shutdown()
         
 def q_job():
@@ -224,8 +209,11 @@ if __name__ == "__main__":
     # add a interval job
     scheduler.add_interval_job(job=q_job, id=project_job_id, seconds=10, exec_immediately=True)
     
+    # list all jobs [dictionary]
+    scheduler.list_jobs_dict()
+
     # remove a job by id
-    scheduler.remove_job(id=project_job_id)
+    scheduler.remove_job(project_job_id)
     
     # shutdown schedule, every job will be pause.
     scheduler.shutdown()
